4d083a1af79b9397e7e0950282bcac106ff2e836
metamn <bartus.csongor@gmail.com>
2019-01-08 18:56:32 +0200

    All cleaned up

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,9 +4,9 @@ import SEO from '../components/SEO'
 
 const NotFoundPage = () => (
 	<>
-	<SEO title="404: Not found" />
-	<h1>NOT FOUND</h1>
-	<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+		<SEO title="404: Not found" />
+		<h1>NOT FOUND</h1>
+		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 	</>
 )
 
